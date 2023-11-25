68dca8f1f145b77b56a8f620a1e681ff52ae53a2
Morgan Jones <me@numin.it>
2023-11-24 17:19:47 -0800

    zhammer: fix test issues

--- nixos/tests/zhammer.nix
+++ nixos/tests/zhammer.nix
@@ -11,9 +11,10 @@ let
     { kernelPackage ? if enableUnstable
                       then pkgs.zfsUnstable.latestCompatibleLinuxPackages
                       else pkgs.linuxPackages
+    , extraKernelParams ? []
     , enableUnstable ? false
-    , enableSystemdStage1 ? false
-    , zfsPackage ? if enableUnstable then pkgs.zfs else pkgs.zfsUnstable
+    , ignoreFailures ? false
+    , zfsPackage ? if enableUnstable then pkgs.zfsUnstable else pkgs.zfs
     , extraTest ? ""
     }:
     makeTest {
@@ -22,7 +23,7 @@ let
         maintainers = [ numinit ];
       };
 
-      nodes.machine = { config, pkgs, lib, ... }: let
+      nodes.${name} = { config, pkgs, lib, ... }: let
         zhammer = pkgs.writeShellScriptBin "zhammer" ''
           set -euo pipefail
 
@@ -74,7 +75,7 @@ let
 
           total=0
           for (( n=0; n<=$count; n+=$check_every )); do
-            log "writing $check_every files"
+            log "writing $check_every files at iteration $n"
             h=0
             for (( i=1; i<=$check_every; i+=2 )); do
               j=$((i+1))
@@ -83,7 +84,7 @@ let
               h=$((h+1))
             done
 
-            log "checking $check_every files"
+            log "checking $check_every files at iteration $n"
             for (( i=1; i<=$check_every; i++ )); do
               old="''${prefix}0"
               copy="''${prefix}$i"
@@ -98,11 +99,12 @@ let
                 exit 1
               fi
             done
+            rm -f "$prefix"[1-9]* || true
           done
         '';
       in {
         virtualisation = {
-          cores = 4;
+          cores = 8;
           memorySize = 8192;
           useBootLoader = true;
           useEFIBoot = true;
@@ -112,9 +114,13 @@ let
         boot.loader.efi.canTouchEfiVariables = true;
         networking.hostId = "deadbeef";
         boot.kernelPackages = kernelPackage;
+
+        # Always enable block cloning if supported.
+        boot.kernelParams = ["zfs.zfs_bclone_enabled=1"] ++ extraKernelParams;
+
         boot.zfs.package = zfsPackage;
         boot.supportedFilesystems = [ "zfs" ];
-        boot.initrd.systemd.enable = enableSystemdStage1;
+        boot.initrd.systemd.enable = false;
 
         environment.systemPackages = [ pkgs.parted pkgs.parallel zhammer ];
 
@@ -123,22 +129,38 @@ let
       };
 
       testScript = ''
+        machine = ${name}
         machine.wait_for_unit("multi-user.target")
         machine.succeed(
           "zpool status",
           "truncate -s 4G /dev/shm/zfs",
-          "zpool create -f -o ashift=13 -O canmount=off -O mountpoint=none -O xattr=sa -O dnodesize=auto -O acltype=posix -O atime=off -O relatime=on tank /dev/shm/zfs",
+          "zpool create -f -o ashift=12 -O canmount=off -O mountpoint=none -O xattr=sa -O dnodesize=auto -O acltype=posix -O atime=off -O relatime=on tank /dev/shm/zfs",
           "zfs create -o canmount=on -o mountpoint=/test tank/test"
         )
 
         machine.succeed(
-          "parallel --lb --halt-on-error now,fail=1 zhammer /test 100000 16k 10000 ::: $(seq $(nproc))"
+          "parallel --lb --halt-on-error now,fail=1 zhammer /test 1000000 16k 10000 ::: $(seq $(nproc))${pkgs.lib.optionalString ignoreFailures " || true"}"
         )
       '' + extraTest;
     };
 
 
 in {
-  stable = makeZfsTest "stable" { };
-  #zfs_2_1_with_patch = makeZfsTest "zfs_2_1_with_patch" { zfsPackage = pkgs.zfs_2_1; };
+  zfs_2_2_1 = makeZfsTest "zfs_2_2_1" { ignoreFailures = true; };
+  zfs_2_2_1_zfs_dmu_offset_next_sync = makeZfsTest "zfs_2_2_1_zfs_dmu_offset_next_sync" { ignoreFailures = true; extraKernelParams = [ "zfs.zfs_dmu_offset_next_sync=0" ]; };
+  /*
+  # For some reason, this fails
+  zfs_2_2_1_with_patch = makeZfsTest "zfs_2_2_1_with_patch" {
+    zfsPackage = pkgs.zfs.overrideAttrs (prev: {
+      extraPatches = [
+        (pkgs.fetchpatch {
+          # https://github.com/openzfs/zfs/pull/15571
+          # Remove when it's backported to 2.1.x.
+          url = "https://github.com/robn/zfs/commit/617c990a4cf1157b0f8332f35672846ad16ca70a.patch";
+          hash = "sha256-j5YSrud7BaWk2npBl31qwFFLYltbut3CUjI1cjZOpag=";
+        })
+      ];
+    });
+  };*/
+  zfs_2_1_with_patch = makeZfsTest "zfs_2_1_with_patch" { zfsPackage = pkgs.zfs_2_1; };
 }
